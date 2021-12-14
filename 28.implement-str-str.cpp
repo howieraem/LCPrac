a6b6fe8c510202d49ef6af91d965f047776ffb02
howieraem <34226262+howieraem@users.noreply.github.com>
2021-12-13 22:55:08 -0500

    update

---
+++ 28.implement-str-str.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=28 lang=cpp
+ *
+ * [28] Implement strStr()
+ */
+#include <string>
+
+using std::string;
+
+// @lc code=start
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        const int m = needle.size();
+        if (!m)  return 0;
+        const int n = haystack.size();
+        
+        // failure function
+        int f[m];
+        memset(f, 0, sizeof(f));
+        int i = 1, j = 0;
+        while (i < m) {
+            if (needle[i] == needle[j]) {
+                f[i++] = ++j;
+            } else if (j > 0) {
+                j = f[j - 1];
+            } else {
+                ++i;
+            }
+        }
+        
+        // KMP
+        i = j = 0;
+        while (i < n) {
+            if (haystack[i] == needle[j]) {
+                if (j == m - 1)  return i - j;
+                else {
+                    ++i; ++j;
+                }
+            } else {
+                if (j)  j = f[j - 1];
+                else  ++i;
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
