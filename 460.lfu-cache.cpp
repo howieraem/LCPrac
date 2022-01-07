3f941db06d8227aa188139c83a4504471c98c75c
howieraem <34226262+howieraem@users.noreply.github.com>
2022-01-07 01:45:38 -0500

    update

--- 460.lfu-cache.cpp
+++ 460.lfu-cache.cpp
@@ -8,29 +8,36 @@
 using namespace std;
 
 // @lc code=start
+// Solution with std::list
+struct Node {
+    int v, freq;
+    list<int>::iterator iter;
+
+    Node(int _v, int _freq, list<int>::iterator _iter) {
+        v = _v;
+        freq = _freq;
+        iter = _iter;
+    }
+};
+
 class LFUCache {
     int cap, sz, curLeastFreq = 0;
 
     // (frequency, list of keys with that frequency)
     unordered_map<int, list<int>> freqs;
-    
-    // (key, iterator that points to the key's position in the corresponding frequency list)
-    unordered_map<int, list<int>::iterator> iters;
 
-    // (key, (value, frequency))
-    unordered_map<int, pair<int, int>> vals;
+    unordered_map<int, Node*> mp;
 
     void update(int key) {
-        int freq = vals[key].second;
-        auto iter = iters[key];
+        Node *node = mp[key];
 
-        // Increment the frequency of the key
-        ++vals[key].second;
-        freqs[freq].erase(iter);
-        freqs[freq + 1].push_back(key);
+        // Erase the key from old frequency list
+        freqs[node->freq].erase(node->iter);
 
-        // Make the key's iterator point to the last added frequency list item 
-        iters[key] = --freqs[freq + 1].end();
+        // Increment frequency and update corresponding list and iterator
+        node->freq++;
+        freqs[node->freq].push_back(key);
+        node->iter = --freqs[node->freq].end();     // iterator points to the back of the frequency list at which the key is
 
         if (freqs[curLeastFreq].empty()) {
             ++curLeastFreq;
@@ -45,31 +52,29 @@ public:
     
     // O(1)
     int get(int key) {
-        if (vals.find(key) == vals.end())  return -1;
+        if (mp.find(key) == mp.end())  return -1;
         update(key);
-        return vals[key].first;
+        return mp[key]->v;
     }
     
     // O(1)
     void put(int key, int value) {
         if (!cap)  return;
-        if (vals.find(key) != vals.end()) {
+        if (mp.find(key) != mp.end()) {
             // Updates value of an existing key
-            vals[key].first = value;
+            mp[key]->v = value;
             update(key);    // increment frequency
         } else {
             if (sz == cap) {    // full, need to pop the least frequent data
-                int evict = freqs[curLeastFreq].front(); freqs[curLeastFreq].pop_front();
-                vals.erase(evict);
-                iters.erase(evict);
+                int evictKey = freqs[curLeastFreq].front(); freqs[curLeastFreq].pop_front();
+                mp.erase(evictKey);
             } else {
                 ++sz;
             }
 
             // Insert new k-v pair
-            vals[key] = {value, 1};
             freqs[1].push_back(key);
-            iters[key] = --freqs[1].end();
+            mp[key] = new Node(value, 1, --freqs[1].end());
             curLeastFreq = 1;
         }
     }
