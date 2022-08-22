59fedb927581a8b9520e901ea55d21e72d8c54e4
howieraem <34226262+howieraem@users.noreply.github.com>
2022-08-22 03:15:35 -0400

    Update 843.guess-the-word.py

--- 843.guess-the-word.py
+++ 843.guess-the-word.py
@@ -14,35 +14,40 @@ class Master:
 class Solution:
     # Reduce calling guess(w) as many as possible. Try to narrow the 
     # candidates after each time we call guess(w).
-    # More explanations: https://leetcode.com/problems/guess-the-word/discuss/556075
+    # However, calling guess(w) less than 10 times is not strictly guaranteed.
+    # More explanations: 
+    #   - https://leetcode.com/problems/guess-the-word/discuss/556075
+    #   - https://leetcode.com/problems/guess-the-word/discuss/134251
     # T: O(n^2 * l), n := initial len(wordlist), l := word length
     # S: O(n)
     def findSecretWord(self, words: List[str], master: 'Master') -> None:
-        N = len(words)
         L = len(words[0])  # 6
-        i = 0
         matches = 0
-        while i < N:
-            candidate = words[random.randint(0, len(words) - 1)]
-            prev_matches = matches
+        while True:
+            # Approach 1: random guess (more likely to fail some test cases)
+            # candidate = words[random.randint(0, len(words) - 1)]
+
+            # Approach 2: histogram-based min-max heuristic
+            hist = {}
+            for w1 in words:
+                hist_w = [0] * 7  # -1, 0, 1, ..., 6
+                for w2 in words:
+                    hist_w[self.get_matches(w1, w2)] += 1
+                hist[w1] = max(hist_w)
+            candidate = min(words, key=lambda w: hist[w])
+
             matches = master.guess(candidate)
             if matches == L:
-                break
-            if matches > 0 and matches <= prev_matches:
-                continue
+                return
             candidates = []
             for w in words:
                 if matches == self.get_matches(candidate, w):
                     candidates.append(w)
             words = candidates
-        return candidate
 
     @staticmethod
     def get_matches(w1, w2):
-        cnt = 0
-        for c1, c2 in zip(w1, w2):
-            cnt += c1 == c2
-        return cnt
+        return sum(c1 == c2 for c1, c2 in zip(w1, w2))
 
 # @lc code=end
 
