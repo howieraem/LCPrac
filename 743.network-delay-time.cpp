131a7f280e971e71365679b11ae1ad8ae4956291
howieraem <34226262+howieraem@users.noreply.github.com>
2022-10-09 00:01:36 -0400

    update

--- 743.network-delay-time.cpp
+++ 743.network-delay-time.cpp
@@ -16,51 +16,55 @@ using namespace std;
 class Solution {
 public:
     /*
-    // Bellman Ford Algorithm: works with negative weights. Dijkstra's algorithm
-    // only works with non-negative weights. However, time complexity is O(V*E).
+    // Bellman-Ford Algorithm: works with negative edge costs/weights
+    // T: O(V * E)
+    // S: O(V)
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<int> dist(n + 1, INT_MAX);   // add 1 because of 1-indexed nodes
         dist[k] = 0;
+        
         for (int i = 0; i < n; ++i) {
-            for (auto &e : times) {
-                int u = e[0], v = e[1], w = e[2];
+            for (const auto &[u, v, w] : times) {
                 if (dist[u] != INT_MAX && dist[v] > dist[u] + w) {
                     dist[v] = dist[u] + w;
                 }
             }
         }
 
-        int ans = 0;
-        for (int i = 1; i <= n; ++i) {
-            ans = max(ans, dist[i]);
-        }
+        int ans = *max_element(dist.begin() + 1, dist.end());  // add 1 to begin() because of 1-indexed nodes
         return ans == INT_MAX ? -1 : ans;
     }
     */
 
-    // Dijkstra's algorithm, O(V + E*log(V))
+    // Dijkstra's Algorithm. Only works with non-negative edge costs/weights.
+    // T: O((V + E) * log(V))
+    // S: O(V + E)
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> g(n + 1);
+        // Build the adjacency list and convert nodes from 1-indexed to 0-indexed
+        --k;
+        vector<vector<pair<int, int>>> adj(n);
         for (const auto &e : times) {
-            g[e[0]].emplace_back(e[1], e[2]);
+            adj[e[0] - 1].emplace_back(e[1] - 1, e[2]);
         }
-        vector<int> dist(n + 1, INT_MAX);
+
+        // Priority queue of (edge cost, edge destination node)
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
+        vector<int> dist(n, INT_MAX);
+
+        // Start from node k
+        min_heap.emplace(0, k);
         dist[k] = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.emplace(0, k);
-        int u, v, w;
-        while (!pq.empty()) {
-            pair<int, int> p = pq.top(); pq.pop();
-            u = p.second;
-            for (const auto &t : g[u]) {
-                v = t.first, w = t.second;
+        
+        while (min_heap.size()) {
+            auto [_, u] = min_heap.top(); min_heap.pop();
+            for (const auto& [v, w] : adj[u]) {
                 if (dist[v] > dist[u] + w) {
                     dist[v] = dist[u] + w;
-                    pq.emplace(dist[v], v);
+                    min_heap.emplace(dist[v], v);
                 }
             }
         }
-        int ans = *max_element(dist.begin() + 1, dist.end());
+        int ans = *max_element(dist.begin(), dist.end());
         return ans == INT_MAX ? -1 : ans;
     }
 };
