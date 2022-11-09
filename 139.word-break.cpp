53994827262183b4f0f129efef1c4919c3075198
howieraem <34226262+howieraem@users.noreply.github.com>
2022-11-08 21:48:48 -0500

    update

---
+++ 139.word-break.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=139 lang=cpp
+ *
+ * [139] Word Break
+ */
+#include <bits/stdc++.h>
+
+using std::string;
+using std::unordered_set;
+using std::vector;
+
+// @lc code=start
+class Solution {
+public:
+    // T: O(n ^ 3)
+    // S: O(n + len(wordDict))
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> words(wordDict.begin(), wordDict.end());
+
+        const int n = s.size();
+
+        // dp[i] means whether s[:i] can be segmented into a space-separated sequence of one or more dictionary words
+        vector<uint8_t> dp(n + 1, 0);
+        dp[0] = 1;  // IMPORTANT base case
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                // dp[i] is true iff dp[j] is true and s[j:i] is in dict.
+                // substr() is O(n)
+                if (dp[j] && words.find(s.substr(j, i - j)) != words.end()) {
+                    dp[i] = 1;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
+// @lc code=end
+
---
+++ 152.maximum-product-subarray.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=152 lang=cpp
+ *
+ * [152] Maximum Product Subarray
+ */
+#include <bits/stdc++.h>
+
+using std::vector;
+
+// @lc code=start
+class Solution {
+public:
+    // T: O(n)
+    // S: O(1)
+    int maxProduct(vector<int>& nums) {
+        int prefix_product = 1, suffix_product = 1;
+
+        // IMPORTANT: The lower bound of ans is not 0. If nums only contain a negative value, ans will be negative.
+        int ans = INT_MIN;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            ans = std::max(ans, prefix_product *= nums[i]);
+            if (prefix_product == 0) {  // or nums[i] == 0
+                prefix_product = 1;
+            }
+        }
+
+        // IMPORTANT: Prefix products may miss the optimum. Need to consider suffix products as well.
+        // Example: nums = [-3, -1, -10], prefix products = [-3, 3, -30], suffix products = [-30, 10, -10]
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            ans = std::max(ans, suffix_product *= nums[i]);
+            if (suffix_product == 0) {  // or nums[i] == 0
+                suffix_product = 1;
+            }
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
+
