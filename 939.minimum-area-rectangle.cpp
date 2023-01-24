4c542a2de711bf1639b87e8d4bcc58698dd89187
howieraem <34226262+howieraem@users.noreply.github.com>
2023-01-24 00:01:03 -0800

    update

--- 939.minimum-area-rectangle.cpp
+++ 939.minimum-area-rectangle.cpp
@@ -26,15 +26,15 @@ public:
         const int n = points.size();
         int ans = INT_MAX;
         for (int i = 0; i < n; ++i) {
-            int x1 = points[i][0], y1 = points[i][1];
+            const int &x1 = points[i][0], &y1 = points[i][1];
             for (int j = i + 1; j < n; ++j) {
-                int x2 = points[j][0], y2 = points[j][1];
+                const int &x2 = points[j][0], &y2 = points[j][1];
 
                 if (x1 != x2 && y1 != y2) {
-                    unordered_set<int> *sy1 = &x2ys[x1], *sy2 = &x2ys[x2];
+                    const unordered_set<int> &sy1 = x2ys[x1], &sy2 = x2ys[x2];
                     // If y2 is in set of x1's y and y1 is in set of x2's y,
                     // the two points form one diagonal of a rectangle.
-                    if (sy1->find(y2) != sy1->end() && sy2->find(y1) != sy2->end()) {
+                    if (sy1.find(y2) != sy1.end() && sy2.find(y1) != sy2.end()) {
                         ans = min(ans, abs(x1 - x2) * abs(y1 - y2));
                     }
                 }
