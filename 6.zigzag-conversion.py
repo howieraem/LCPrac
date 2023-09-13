df5548d3f1cc1c95964885c3e37d8fad15dcf46f
howieraem <34226262+howieraem@users.noreply.github.com>
2023-09-13 00:16:59 -0700

    update

---
+++ 6.zigzag-conversion.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=6 lang=python3
+#
+# [6] Zigzag Conversion
+#
+
+# @lc code=start
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2:
+            return s
+        res = ["" for _ in range(numRows)]
+        row_idx = 0
+        row_dirn = -1
+        LAST = numRows - 1
+        for c in s:
+            res[row_idx] += c
+            if row_idx == 0 or row_idx == LAST:
+                # flip sign to reverse the direction
+                row_dirn = (~row_dirn + 1)
+            row_idx += row_dirn
+        return "".join(res)
+
+# @lc code=end
+
